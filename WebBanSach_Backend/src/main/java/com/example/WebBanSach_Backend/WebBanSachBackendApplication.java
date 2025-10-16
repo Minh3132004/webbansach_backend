9a3f7e1344ec4aef5e3c3714618acf1aea60ada4
Minh3132004 <vubinhminh04@gmail.com>
2025-10-16 12:52:38 +0700

    Back-end: Khởi tạo dự án

---
+++ WebBanSach_Backend/src/main/java/com/example/WebBanSach_Backend/WebBanSachBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebBanSach_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBanSachBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBanSachBackendApplication.class, args);
+	}
+
+}
---
+++ WebBanSach_Backend/src/test/java/com/example/WebBanSach_Backend/WebBanSachBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebBanSach_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBanSachBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
