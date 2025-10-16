68b09eb024a597c0d4b0f4201eddca23aba2cddf
Minh3132004 <vubinhminh04@gmail.com>
2025-10-16 21:57:34 +0700

    Back-end: Cấu hình cho phép hoặc chặn các phương thức

--- WebBanSach_Backend/src/main/java/com/example/WebBanSach_Backend/Config/RepositoryConfig.java
+++ WebBanSach_Backend/src/main/java/com/example/WebBanSach_Backend/Config/RepositoryConfig.java
@@ -1,11 +1,14 @@
 package com.example.WebBanSach_Backend.Config;
 
+import com.example.WebBanSach_Backend.Entity.NguoiDung;
+import com.example.WebBanSach_Backend.Entity.TheLoai;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.metamodel.Type;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
@@ -15,10 +18,42 @@ public class RepositoryConfig implements RepositoryRestConfigurer {
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+        // Cho phép trả về id
+
         Class[] classes = entityManager.getMetamodel()
                 .getEntities().stream()
                 .map(Type::getJavaType)
                 .toArray(Class[]::new);
         config.exposeIdsFor(classes);
+
+
+        // Chặn các methods
+        HttpMethod[] chanCacPhuongThuc ={
+                HttpMethod.POST,
+                HttpMethod.PUT,
+                HttpMethod.PATCH,
+                HttpMethod.DELETE,
+        };
+
+        //Không thể thêm, sửa, xóa thể loại
+
+        disableHttpMethods(TheLoai.class, config, chanCacPhuongThuc);
+
+        // Chặn các method DELETE cho người dùng
+        HttpMethod[] phuongThucDelete = {
+                HttpMethod.DELETE
+        };
+        disableHttpMethods(NguoiDung.class, config,phuongThucDelete );
     }
+
+    private void disableHttpMethods(Class c,
+                                    RepositoryRestConfiguration config,
+                                    HttpMethod[] methods){
+        config.getExposureConfiguration()
+                .forDomainType(c)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(methods))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(methods));
+    }
+
 }
