2d1ea54502df5d7a37ee7a87884b3be80dfb5803
Minh3132004 <vubinhminh04@gmail.com>
2025-10-24 18:36:10 +0700

    Fullstack: Kiểm tra quyền truy cập tại Front-end

--- WebBanSach_Backend/src/main/java/com/example/WebBanSach_Backend/Service/JwtService.java
+++ WebBanSach_Backend/src/main/java/com/example/WebBanSach_Backend/Service/JwtService.java
@@ -3,6 +3,7 @@ package com.example.WebBanSach_Backend.Service;
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -11,16 +12,45 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+
+import com.example.WebBanSach_Backend.Entity.NguoiDung;
+import com.example.WebBanSach_Backend.Entity.Quyen;
 
 @Component
 public class JwtService  {
     public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
 
+    @Autowired
+    private UserService userService ; 
+
     //Tạo JWT dựa trên tên đăng nhập 
     public String generateToken(String tenDangNhap){
         Map<String, Object> claims = new HashMap<>();
+        boolean isAdmin = false;
+        boolean isStaff = false;
+        boolean isUser = false;
+        NguoiDung nguoiDung = userService.findByUsername(tenDangNhap);
+        if(nguoiDung != null && nguoiDung.getDanhSachQuyen().size()>0){
+            List<Quyen> listQuyen = nguoiDung.getDanhSachQuyen();
+            for(Quyen quyen : listQuyen){
+                if(quyen.getTenQuyen().equals("ADMIN")){
+                    isAdmin = true;
+                }
+                if(quyen.getTenQuyen().equals("STAFF")){
+                    isStaff = true;
+                }
+                if(quyen.getTenQuyen().equals("USER")){
+                    isUser = true;
+                }
+            }
+        }
+        claims.put("isAdmin", isAdmin);
+        claims.put("isStaff", isStaff);
+        claims.put("isUser", isUser);
         return createToken(claims, tenDangNhap);
     }
 
